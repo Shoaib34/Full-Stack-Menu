b86031e8344cf92f61cef7aec36c6d534cd1f166
Mohammad <ilahimohammad8@gmail.com>
2023-10-16 11:10:36 -0400

    Update front menu.js

--- front menu.js
+++ front menu.js
@@ -1,51 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Menu() {
-  // Define an array of menu items
-  const menuItems = [
-    {
-      id: 1,
-      name: 'Burger',
-      description: 'A delicious burger with all the fixings.',
-      price: 10.99,
-    },
-    {
-      id: 2,
-      name: 'Pizza',
-      description: 'A classic pizza with your favorite toppings.',
-      price: 12.99,
-    },
-    {
-      id: 3,
-      name: 'Pasta',
-      description: 'Spaghetti with marinara sauce and meatballs.',
-      price: 9.99,
-    },
-    // You can add more menu items here
-    {
-      id: 4,
-      name: 'Salad',
-      description: 'Fresh garden salad with choice of dressing.',
-      price: 7.99,
-    },
-    {
-      id: 5,
-      name: 'Steak',
-      description: 'Juicy steak cooked to perfection.',
-      price: 15.99,
-    },
-  ];
+  // Define a state variable to hold menu items
+  const [menuItems, setMenuItems] = useState([]);
+
+  // Fetch menu items from the back end when the component mounts
+  useEffect(() => {
+    // Make a GET request to your back-end API to fetch menu items
+    axios.get('/api/menu') // Replace with your actual API endpoint
+      .then((response) => {
+        // Set the 'menuItems' state with the data received from the back end
+        setMenuItems(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching menu items:', error);
+      });
+  }, []);
+
+  // Function to handle placing an order for a menu item
+  const handleOrder = (menuItem) => {
+    // Make a POST request to your back-end API to place an order
+    axios.post('/api/order', { item: menuItem.item })
+      .then((response) => {
+        // Handle the response (e.g., show a confirmation message)
+        console.log('Order placed successfully:', response.data);
+      })
+      .catch((error) => {
+        console.error('Error placing the order:', error);
+      });
+  };
 
   return (
     <div>
       <h2>Menu</h2>
       <ul>
-        {menuItems.map((item) => (
-          <li key={item.id}>
-            <h3>{item.name}</h3>
-            <p>{item.description}</p>
-            <p>${item.price.toFixed(2)}</p>
-          </li>
+        {/* Map through the 'menuItems' array and create a button for each item */}
+        {menuItems.map((menuItem) => (
+          <button key={menuItem._id} onClick={() => handleOrder(menuItem)}>
+            {menuItem.item}
+          </button>
         ))}
       </ul>
     </div>
