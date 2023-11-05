61051068cee40f870101611851315bf699a00138
Mohammad <ilahimohammad8@gmail.com>
2023-11-05 12:02:33 -0500

    Update backend Orders.js

--- backend Orders.js
+++ backend Orders.js
@@ -1,26 +1,56 @@
-// orders.js
+// Import required packages
 const express = require('express');
-const router = express.Router();
-const Order = require('../models/Order'); // Import the Order model
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-// Route to place an order
-router.post('/', async (req, res) => {
-  // Extract the data from the request body
-  const { foodItemId, quantity } = req.body;
+// Create an Express application
+const app = express();
 
+// Define the port the server will listen on
+const port = process.env.PORT || 3000;
+
+mongoose.connect('mongodb://127.0.0.1:27017/food', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected successfully');
+})
+.catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+// Create a basic model and schema for the "food" collection in MongoDB
+const menuSchema = new mongoose.Schema({
+  item: String,
+  description: String,
+  price: Number,
+  quantity: Number,
+});
+
+const Menu = mongoose.model('order', menuSchema);
+
+// Enable CORS (Cross-Origin Resource Sharing) to allow requests from other domains
+app.use(cors());
+
+// Parse JSON data in incoming requests
+app.use(express.json());
+
+// Define your API routes (e.g., an endpoint to get a list of menu items)
+app.get('/api/order', async (req, res) => {
   try {
-    // Create a new order based on the model and request data
-    const newOrder = new Order({ foodItemId, quantity });
-    
-    // Save the order in the database
-    await newOrder.save();
-
-    // Respond with the new order and a success status
-    res.status(201).json(newOrder);
+    // Fetch all menu items from the MongoDB database using Mongoose
+    const menuItems = await Menu.find({});
+    res.json(menuItems);
+    console.log('Menu items fetched from the database:', menuItems);
+    // Respond with the menu items as a JSON array
   } catch (error) {
-    // If an error occurs, respond with an error status and message
-    res.status(500).json({ error: 'Failed to place order' });
+    console.error('Error fetching menu items:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-module.exports = router;
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
