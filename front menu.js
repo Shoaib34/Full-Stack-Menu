b9a37e7028d09bb9a3041781d361d37d2d3528c2
Mohammad <ilahimohammad8@gmail.com>
2023-11-12 10:39:49 -0500

    Update front menu.js

--- front menu.js
+++ front menu.js
@@ -4,42 +4,48 @@ import './Menu.css'; // Import the CSS file for styling
 
 function Menu() {
   const [menuItems, setMenuItems] = useState([]);
-  const [selectedItems, setSelectedItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
-  const handleItemClick = (menuItem) => {
-    const itemIndex = selectedItems.findIndex((item) => item._id === menuItem._id);
+  const increaseQuantity = (menuItem) => {
+    // Increment quantity for the selected menu item in both menuItems and cartItems
+    const updatedMenuItems = menuItems.map((item) =>
+      item._id === menuItem._id ? { ...item, quantity: item.quantity + 1 } : item
+    );
+    setMenuItems(updatedMenuItems);
 
-    if (itemIndex !== -1) {
-      const updatedSelectedItems = [...selectedItems];
-      updatedSelectedItems[itemIndex] = {
-        ...selectedItems[itemIndex],
-        quantity: selectedItems[itemIndex].quantity + 1,
-      };
-      setSelectedItems(updatedSelectedItems);
-    } else {
-      setSelectedItems([...selectedItems, { ...menuItem, quantity: 1 }]);
-    }
-    console.log(`Added ${menuItem.item} to cart.`);
+    const updatedCartItems = cartItems.map((item) =>
+      item._id === menuItem._id ? { ...item, quantity: item.quantity + 1 } : item
+    );
+    setCartItems(updatedCartItems);
   };
 
-  const increaseQuantity = (selectedItem) => {
-    const updatedSelectedItems = selectedItems.map((item) =>
-      item._id === selectedItem._id ? { ...item, quantity: item.quantity + 1 } : item
+  const decreaseQuantity = (menuItem) => {
+    // Decrement quantity for the selected menu item in both menuItems and cartItems
+    const updatedMenuItems = menuItems.map((item) =>
+      item._id === menuItem._id ? { ...item, quantity: Math.max(0, item.quantity - 1) } : item
     );
-    setSelectedItems(updatedSelectedItems);
-  };
+    setMenuItems(updatedMenuItems);
 
-  const decreaseQuantity = (selectedItem) => {
-    const updatedSelectedItems = selectedItems.map((item) =>
-      item._id === selectedItem._id ? { ...item, quantity: Math.max(0, item.quantity - 1) } : item
+    const updatedCartItems = cartItems.map((item) =>
+      item._id === menuItem._id ? { ...item, quantity: Math.max(0, item.quantity - 1) } : item
     );
-    setSelectedItems(updatedSelectedItems);
+    setCartItems(updatedCartItems);
   };
 
   const addToCart = () => {
-    console.log('Selected Items:', selectedItems);
+    console.log("Added items to cart:", cartItems);
+    // Send a POST request to the backend with the cartItems
+    axios.post('http://localhost:3000/api/order', { cartItems })
+      .then((response) => {
+        console.log("Cart items sent to the server:", response.data);
+        // Perform additional actions after successfully sending data
+      })
+      .catch((error) => {
+        console.error('Error sending cart items:', error);
+      });
   };
 
+  // Fetch menu items from the backend when the component mounts
   useEffect(() => {
     axios.get('http://localhost:3000/api/menu')
       .then((response) => {
@@ -54,6 +60,7 @@ function Menu() {
     <div>
       <h2>Menu</h2>
       <div className="menu-items-container">
+        {/* Display menu items with buttons to increase/decrease quantity */}
         {menuItems.map((menuItem) => (
           <div key={menuItem._id} className="menu-item">
             <p>{menuItem.item}</p>
@@ -61,18 +68,16 @@ function Menu() {
             <p>${menuItem.price}</p>
             <p>Quantity: {menuItem.quantity}</p>
             <div className="button-group">
-              <button onClick={() => handleItemClick(menuItem)}>Add to Cart</button>
               <button onClick={() => increaseQuantity(menuItem)}>+</button>
               <button onClick={() => decreaseQuantity(menuItem)}>-</button>
             </div>
           </div>
         ))}
       </div>
-      {selectedItems.length > 0 && (
-        <button className="bottom-right-button" onClick={addToCart}>
-          Add Selected Items to Cart
-        </button>
-      )}
+      {/* Button to add selected items to the cart */}
+      <button className="bottom-right-button" onClick={addToCart}>
+        Add Selected Items to Cart
+      </button>
     </div>
   );
 }
